ce7abfa67d5e68c44a50961cc6b9d86f46200e76
Ahmed Wael <notahmedwael888@gmail.com>
2025-11-20 09:50:42 +0200

    Added Comments

--- Swap/Swap.cpp
+++ Swap/Swap.cpp
@@ -1,21 +1,21 @@
 #include "Swap.h"
 
 void swapDefault(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
+    int temp = a; // Temp to hold variable
+    a = b; // b becomes original a
+    b = temp; // a becomes original b
 }
 
 void swapXOR(int &a, int &b) {
     if (&a != &b) { // Check for self-swap, XORing a number with itself results in 0
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
+        a = a ^ b; // a now holds (a XOR b)
+        b = a ^ b; // b becomes original a: (a XOR b) XOR b = a
+        a = a ^ b; // a becomes original b: (a XOR b) XOR a = b
     }
 }
 
 void swapAddSub(int &a, int &b) {
-    a = a + b;
-    b = a - b;
-    a = a - b;
+    a = a + b; // a now holds sum of both
+    b = a - b; // b becomes original a: (a+b) - b = a
+    a = a - b; // a becomes original b: (a+b) - a = b
 }
